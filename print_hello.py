fea5ab810d90fc6170e73e0f736d5973a16a6463
Taesoo-Park <110900118+Taesoo-Park@users.noreply.github.com>
2022-08-11 14:28:23 +0900

    Add files via upload

---
+++ print_hello.py
@@ -0,0 +1,11 @@
+print('Hello, Python!')
+print(1+3)
+x=10; y=11; z=12
+print(x+y+z)
+print('x+y+z=',x+y+z)
+print('x=',x)
+print('y=',y)
+print('z=',z)
+axis=x+y+z
+print('axis',axis)
+# Practise python
